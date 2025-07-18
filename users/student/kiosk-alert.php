085a65ece99a5ae0045f4aa1c16cb577329ecd2a
Joseph <buenaflorjrjoseph@immaculada.edu.ph>
2025-07-18 19:56:03 +0800

    pota

--- users/student/kiosk-alert.php
+++ users/student/kiosk-alert.php
@@ -3,8 +3,9 @@
     <div class="logo-modal">
 
     </div>
+    <div class="Header">Immaculada Concepcion College of Soldiers Hills Inc.</div>
     <div class="content">
-      
+      Queue reservation is currently unavailable. You may reserve queue number between 7:30 AM - 5:00 PM Thank you for your patience.
     </div>
 
   </div>
